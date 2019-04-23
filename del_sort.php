ec00b59276c0bdf11d421bb13a6a85227dfda1d1
adonis <adonis.di@toursforfun.cn>
2019-04-23 15:47:49 +0800

    删除排序数组中的重复项

---
+++ del_sort.php
@@ -0,0 +1,11 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+        $nums = array_unique($nums);
+        return count ($nums);
+    }
+}
