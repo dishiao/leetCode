5c4a340c0008ce99c65404cde5fa66395b1a1286
adonis <adonis.di@toursforfun.cn>
2019-04-23 16:17:41 +0800

    二叉树的最大深度

---
+++ max_deep_tree.php
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    static function maxDepth($root) {
+        // 1 2 4 8 16 32 68...
+        if ($root == null) return 0;
+        return max([static::maxDepth($root->left),static::maxDepth($root->right)])+1;
+        
+    }
+}
