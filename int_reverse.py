1327d88d9e80ee1e08f713d7919130294651c3fd
adonis <adonis.di@toursforfun.cn>
2019-04-23 10:06:59 +0800

    整数翻转 Python

---
+++ int_reverse.py
@@ -0,0 +1,25 @@
+ass Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if -10<x<10:
+            return x
+        str_x = str (x)
+        if str_x[0] == '-':
+            str_new = str_x[1:][::-1]
+            str_new = - int(str_new)
+        else:
+            str_new = str_x[::-1]
+            str_new = int (str_new)
+        
+        # 溢出
+        #if -2147483648<str_new<2147483647:
+        #    str_new = str_new
+        #else:
+        #    str_new = 0
+        #
+        #return str_new
+        
+        return str_new if  -2147483648<str_new<2147483647 else 0
